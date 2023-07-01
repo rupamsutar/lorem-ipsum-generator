b950e0e620e13606dda8d1ee2c7e557e9dade573
Rupam Sutar <rupamsutar281998@gmail.com>
2023-07-01 10:40:19 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,39 @@
+import { useState } from "react";
+import data from "./data";
+
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setText(data.slice(0,count));
+    console.log(text);    
+  }
+
+  return (
+    <section className="section-center">
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraph:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min="1"
+          max="8"
+          step="1"
+          value={count}
+          onChange={(e) => setCount(parseInt(e.target.value))}
+        />
+        <button className="btn" type='submit'>Generate</button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
+    </section>
+  );
 };
 export default App;
